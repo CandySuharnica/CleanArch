e53478c4cd1da390de6081b2bdec5f817e65ec0a
ilia <ilia.rodkin@gmail.com>
2022-06-10 14:31:17 +0300

    add useCases for remove

---
+++ domain/src/main/java/dev/rodkin/domain/useCasesImpl/CatalogUseCasesImpl.kt
@@ -0,0 +1,21 @@
+package dev.rodkin.domain.useCasesImpl
+
+import dev.rodkin.domain.entities.CatalogItem
+import dev.rodkin.domain.repositoryIntefaces.CatalogRepository
+import dev.rodkin.domain.useCases.CatalogUseCases
+import dev.rodkin.domain.utils.ResponseCatalog
+import javax.inject.Inject
+
+class CatalogUseCasesImpl @Inject constructor(
+    private val catalogRepository: CatalogRepository
+) : CatalogUseCases {
+
+    override suspend fun getCatalogList(): List<CatalogItem> {
+        val response = catalogRepository.getCatalogListFromRemove()
+
+        when (response) {
+            is ResponseCatalog.Success -> return response.data
+            is ResponseCatalog.Error -> TODO()
+        }
+    }
+}
